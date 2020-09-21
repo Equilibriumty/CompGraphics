fa7fb18765858898883d72355b8fa835a951846f
Equilibriumty <fanflackplay@gmail.com>
2020-09-21 21:25:35 +0300

    fix x and y limits

--- Lab1/Lab1_task3.py
+++ Lab1/Lab1_task3.py
@@ -1,20 +1,21 @@
 from matplotlib import pyplot as plt
 import numpy as np
 import math
-
+plt.axes()
 
 def draw_lines():
     plt.xlabel("X")
     plt.ylabel("Y")
+    plt.xlim(0, 1)
+    plt.ylim(-0.2, 0.2)
+    plt.grid()
     x = np.linspace(-1, 1, num=1000)
-    plt.grid(axis="both")
-    y1 = np.vectorize(lambda x: 0.0011 * math.acos(x))
-    y2 = np.vectorize(lambda x: 0.0011 * math.atan(x))
-    y3 = np.vectorize(lambda x: 0.0011 * math.asin(x))
+    y1 = np.vectorize(lambda x: 0.11 * math.acos(x))
+    y2 = np.vectorize(lambda x: 0.11 * math.atan(x))
+    y3 = np.vectorize(lambda x: 0.11 * math.asin(x))
     plt.plot(x, y1(x))
     plt.plot(x, y2(x))
     plt.plot(x, y3(x))
     plt.show()
-
 
 draw_lines()
